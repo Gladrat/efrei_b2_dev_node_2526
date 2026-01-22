2188a330e227aad1931b342331d4389961326a38
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-22 08:31:27 +0100

    update

--- cours/io/app.js
+++ cours/io/app.js
@@ -1,2 +1,7 @@
 // Créer une fonction getRandomUser
-  // Interroge l'URL : https://randomuser.me/api
+  // Interroge l'URL avec fetch : https://randomuser.me/api
+  // Récupère le JSON généré par l'API et le retourne
+
+// Créer une fonction writeRandomUser
+  // Prend un utilisateur en entrée
+  // Ecrit cet utilisateur dans un fichier user.json
