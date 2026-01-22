89445584d1ec2293ae6e7d38cfe2072e437fa786
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-22 09:40:41 +0100

    update

--- cours/io/app.js
+++ cours/io/app.js
@@ -1,7 +1,23 @@
-// Créer une fonction getRandomUser
-  // Interroge l'URL avec fetch : https://randomuser.me/api
-  // Récupère le JSON généré par l'API et le retourne
+import { writeFile, readFile } from "node:fs/promises";
 
-// Créer une fonction writeRandomUser
-  // Prend un utilisateur en entrée
-  // Ecrit cet utilisateur dans un fichier user.json
+async function getRandomUser() {
+  const response = await fetch("https://randomuser.me/api");
+  if (response.status == 200) {
+    const data = await response.json();
+    return data;
+  }
+}
+
+async function writeRandomUser() {
+  const user = await getRandomUser();
+
+  const data = JSON.parse(await readFile("./user.json"))
+  data.push(user)
+
+  console.log("Ecriture du fichier sur le disque.");
+  await writeFile("./user.json", JSON.stringify(data));
+}
+
+await writeRandomUser();
+
+console.log("LA SUITE DU SCRIPT !!");
