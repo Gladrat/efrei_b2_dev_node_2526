4604a8f2cc7e333e65025a227d1075b3aee3b542
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-23 10:30:49 +0100

    update

--- web_api/01.monolithique/backend/server.js
+++ web_api/01.monolithique/backend/server.js
@@ -4,6 +4,7 @@ import { readFile } from "node:fs/promises";
 const heroes = ["SuperCORS"];
 
 const app = express();
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   const html = await readFile("../index.html", { encoding: "utf8" });
@@ -11,14 +12,21 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/index.js", async (req, res) => {
+  console.log("Liste des héros:", heroes);
+
   const js = await readFile("../index.js", { encoding: "utf8" });
   res.type("application/javascript");
   res.send(js);
 });
 
 app.get("/heroes", (req, res) => {
-  res.send(heroes)
-})
+  res.send(heroes);
+});
+
+app.post("/heroes", (req, res) => {
+  heroes.push(req.body.hero);
+  res.send("Le héro a bien été ajouté:", req.body.hero);
+});
 
 app.listen(3000, () => {
   console.log("Serveur launched at http://localhost:3000");
