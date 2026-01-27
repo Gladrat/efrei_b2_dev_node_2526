4019a4f7a6e380225ad862d789c08bfd3cda5657
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-27 16:27:24 +0100

    update

--- web_api/heroes-api/src/services/hero.service.js
+++ web_api/heroes-api/src/services/hero.service.js
@@ -3,3 +3,15 @@ import { HeroRepository } from "../repositories/index.js";
 export async function getHeroById(id) {
   return await HeroRepository.getHeroById(id)
 }
+
+export async function getAllHeroes() {
+
+}
+
+export async function createHero() {
+
+}
+
+export async function deleteHero() {
+  
+}
