95f86b77972158990d860f101ba651de5f9ee45b
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-27 14:33:28 +0100

    update

--- web_api/heroes-api/src/config/index.js
+++ web_api/heroes-api/src/config/index.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: "../heroDB.sqlite3",
-  // logging: false
+  logging: false
 })
 
 export default sequelize;
--- web_api/heroes-api/src/server.js
+++ web_api/heroes-api/src/server.js
@@ -1,3 +1,5 @@
+import express from "express";
+
 import sequelize from "./config/index.js";
 import Hero from "./models/hero.model.js";
 
@@ -22,3 +24,22 @@ console.log("La base de données est synchro !");
 // console.log(JSON.stringify(r));
 
 // const destroyedHero = await r.destroy()
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(`<h1>Welcome on S.H.I.E.L.D API</h1>
+<h2>The API is located on <a href="/api/v1">/api/v1</a><h2>`);
+});
+
+app.get("/api/v1", (req, res) => {
+  res.json({message: "S.H.I.E.L.D API is working."})
+})
+
+// Créer une route /api/v1/heroes
+  // Elle doit renvoyer tous les supers héros de la BDD
+
+app.listen(port, () => {
+  console.log(`Server launched at http://localhost:${port}`);
+});
