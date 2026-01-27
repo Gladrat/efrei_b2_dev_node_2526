113dbb7ccd2f0542612fb41b5d4d42b3d43ca2ef
Geoffroy Ladrat <3368584+Gladrat@users.noreply.github.com>
2026-01-27 14:44:28 +0100

    update

--- web_api/heroes-api/src/server.js
+++ web_api/heroes-api/src/server.js
@@ -6,12 +6,21 @@ import Hero from "./models/hero.model.js";
 await sequelize.sync({ force: true });
 console.log("La base de données est synchro !");
 
-// const b = {
-//   alias: "Batman",
-//   identity: "Bruce Wayne",
-//   powerDate: "01/01/2026"
-// }
-// const newHero = await Hero.create(b)
+await Hero.create({
+  alias: "Batman",
+  identity: "Bruce Wayne",
+  powerDate: "01/01/2026",
+});
+await Hero.create({
+  alias: "Joker",
+  identity: "Bruce Wayne",
+  powerDate: "01/01/2026",
+});
+await Hero.create({
+  alias: "SuperNode",
+  identity: "Ryan Dhal",
+  powerDate: "01/01/2009",
+});
 // console.log(newHero.id);
 
 // const batman = await Hero.findByPk(newHero.id)
@@ -28,17 +37,20 @@ console.log("La base de données est synchro !");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome on S.H.I.E.L.D API</h1>
 <h2>The API is located on <a href="/api/v1">/api/v1</a><h2>`);
 });
 
 app.get("/api/v1", (req, res) => {
-  res.json({message: "S.H.I.E.L.D API is working."})
-})
+  res.json({ message: "S.H.I.E.L.D API is working." });
+});
 
-// Créer une route /api/v1/heroes
-  // Elle doit renvoyer tous les supers héros de la BDD
+app.get("/api/v1/heroes", async (req, res) => {
+  res.json(await Hero.findAll());
+});
 
 app.listen(port, () => {
   console.log(`Server launched at http://localhost:${port}`);
